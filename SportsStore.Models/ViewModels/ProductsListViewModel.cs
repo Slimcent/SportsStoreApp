c224fc90631f4b4284a176b86f323d7ae422fc04
Slimcent <centpresley@gmail.com>
2023-07-16 11:47:40 +0100

    View model folder.

---
+++ SportsStore.Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using SportsStore.Models.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsStore.Models.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+            = Enumerable.Empty<Product>();
+        public PagingInfo PagingInfo { get; set; } = new();
+    }
+}
