4c9de794a491925b104bc5a8e099d189a03f9d87
Slimcent <centpresley@gmail.com>
2023-07-21 21:51:24 +0100

    Order repository.

---
+++ SportsStore.Repository/Interfaces/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using SportsStore.Models.Entities;
+using System.Linq;
+
+namespace SportsStore.Repository.Interfaces
+{
+    public interface IOrderRepository
+    {
+        IQueryable<Order> Orders { get; }
+        void SaveOrder(Order order);
+    }
+}
---
+++ SportsStore.Repository/Repositories/OrderRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsStore.Data.Context;
+using SportsStore.Models.Entities;
+using SportsStore.Repository.Interfaces;
+using System.Linq;
+
+namespace SportsStore.Repository.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private SportsStoreDbContext _dbContext;
+
+        public OrderRepository(SportsStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IQueryable<Order> Orders => _dbContext.Orders.Include(o => o.Lines)
+            .ThenInclude(l => l.Product);
+
+        public void SaveOrder(Order order)
+        {
+            _dbContext.AttachRange(order.Lines.Select(l => l.Product));
+            if (order.OrderID == 0)
+            {
+                _dbContext.Orders.Add(order);
+            }
+            _dbContext.SaveChanges();
+        }
+    }
+}
