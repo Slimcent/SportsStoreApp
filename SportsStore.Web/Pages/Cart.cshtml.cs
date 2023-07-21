98f8b6336686c6c8ceb0d8d638aefda150ba15ca
Slimcent <centpresley@gmail.com>
2023-07-21 18:34:17 +0100

    Created cart razor view.

--- SportsStore.Web/Pages/Cart.cshtml.cs
+++ SportsStore.Web/Pages/Cart.cshtml.cs
@@ -1,10 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SportsStore.Web.Infrastructure;
-using SportsStore.Models;
 using SportsStore.Repository.Interfaces;
 using SportsStore.Models.Entities;
 using System.Linq;
+using SportsStore.Web.Services.Implementation;
 
 namespace SportsStore.Web.Pages
 {
@@ -15,27 +15,29 @@ namespace SportsStore.Web.Pages
         public Cart? Cart { get; set; }
         public string ReturnUrl { get; set; } = "/";
 
-        public CartModel(IStoreRepository repo)
+        public CartModel(IStoreRepository repo, Cart cartService)
         {
             repository = repo;
+            Cart = cartService;
         }
 
         public void OnGet(string returnUrl)
         {
             ReturnUrl = returnUrl ?? "/";
-            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
         }
 
         public IActionResult OnPost(long productId, string returnUrl)
         {
-            Product? product = repository.Products
-                .FirstOrDefault(p => p.ProductID == productId);
-            if (product != null)
-            {
-                Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
-                Cart.AddItem(product, 1);
-                HttpContext.Session.SetJson("cart", Cart);
-            }
+            Product product = repository.Products
+            .FirstOrDefault(p => p.ProductID == productId);
+            Cart.AddItem(product, 1);
+            return RedirectToPage(new { returnUrl = returnUrl });
+        }
+
+        public IActionResult OnPostRemove(long productId, string returnUrl)
+        {
+            Cart.RemoveLine(Cart.Lines.First(cl =>
+            cl.Product.ProductID == productId).Product);
             return RedirectToPage(new { returnUrl = returnUrl });
         }
     }
