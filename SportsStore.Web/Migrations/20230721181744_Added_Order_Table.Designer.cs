0f3282c562855e4dd8f2810e443ba2e57a837824
Slimcent <centpresley@gmail.com>
2023-07-21 21:50:20 +0100

    Created order table.

--- SportsStore.Web/Migrations/20230721181744_Added_Order_Table.Designer.cs
+++ SportsStore.Web/Migrations/20230721181744_Added_Order_Table.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SportsStore.Data.Context;
 
 namespace SportsStore.Web.Migrations
 {
     [DbContext(typeof(SportsStoreDbContext))]
-    partial class SportsStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230721181744_Added_Order_Table")]
+    partial class Added_Order_Table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
