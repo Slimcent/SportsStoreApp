27a7264b44ff891d4cdb9a0b9281c5f8d46b5fee
Slimcent <centpresley@gmail.com>
2023-07-22 12:15:07 +0100

    Added shipped property.

--- SportsStore.Models/Entities/Order.cs
+++ SportsStore.Models/Entities/Order.cs
@@ -24,5 +24,7 @@ namespace SportsStore.Models.Entities
         [Required(ErrorMessage = "Please enter a country name")]
         public string Country { get; set; }
         public bool GiftWrap { get; set; }
+        [BindNever]
+        public bool Shipped { get; set; }
     }
 }
---
+++ SportsStore.Web/Migrations/20230722104228_Added_Shipped_Property_To_Orders_Table.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SportsStore.Web.Migrations
+{
+    public partial class Added_Shipped_Property_To_Orders_Table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Shipped",
+                table: "Orders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Shipped",
+                table: "Orders");
+        }
+    }
+}
