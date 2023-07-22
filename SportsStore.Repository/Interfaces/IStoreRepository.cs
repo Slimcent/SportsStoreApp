0c3eda916b98e8cbfcd8f0afc65cc3e9eed9bb8b
Slimcent <centpresley@gmail.com>
2023-07-22 12:55:22 +0100

    Added methods for save, delete and edit.

--- SportsStore.Repository/Interfaces/IStoreRepository.cs
+++ SportsStore.Repository/Interfaces/IStoreRepository.cs
@@ -6,5 +6,8 @@ namespace SportsStore.Repository.Interfaces
     public interface IStoreRepository
     {
         IQueryable<Product> Products { get; }
+        void SaveProduct(Product p);
+        void CreateProduct(Product p);
+        void DeleteProduct(Product p);
     }
 }
